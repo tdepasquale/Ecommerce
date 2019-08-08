c04e519d674ffe61e557fe18e1c6674cbbd72e33
tdepasquale <t.depasquale@hotmail.com>
2019-08-08 17:28:55 -0400

    Add project files.

---
+++ React-Test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace React_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
